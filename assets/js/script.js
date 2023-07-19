a5130622327e638431b98bedee075a5f90b147b3
UlysseLionel <dmitriov.bj@gmail.com>
2023-07-19 19:59:51 +0100

    modification

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,11 +17,13 @@ navToggle.addEventListener('click', () =>{
 
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
+
 const linkAction = () =>{
-    const navMenu= document.getElementById('nav-menu')
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n.addEventListener('click, linkAction'))
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 
@@ -51,7 +53,7 @@ let swiperProjects = new Swiper(".projects__container", {
 
 
 /*=============== SWIPER TESTIMONIAL ===============*/
-let swiperTestimonial = new Swiper(".testimonial__container", {
+let testimonialSwiper = new Swiper(".testimonial__container", {
 grabCursor: true,
 navigation: {
   nextEl: ".swiper-button-next",
@@ -64,12 +66,15 @@ navigation: {
 
 
 /*=============== EMAIL JS ===============*/
+
+
+
+
 const contactForm = document.getElementById('contact-form'),
       contactName = document.getElementById('contact-name'),
       contactEmail = document.getElementById('contact-email'),
       contactProject = document.getElementById('contact-project'),
       contactMessage = document.getElementById('contact-message')
-
 const sendEmail = (e) => {
 e.preventDefault()
 //check if the field has a value
@@ -148,23 +153,64 @@ window.addEventListener('scroll', scrollUp)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 /*=============== DARK LIGHT THEME ===============*/ 
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+//Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+//we obtain the current theme that the interface has by validating dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+//wevalidate if the user previously choose a topic
+if (selectedTheme) {
+  //If the validation is fulfilled
+document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+
+themeButton.addEventListener('click', () => {
+
+document.body.classList.toggle(darkTheme)
+themeButton.classList.toggle(iconTheme)
+
+localStorage.setItem('selected-theme', getCurrentTheme())
+localStorage.setItem('selected-icon', getCurrentIcon())
+
+})
+
+
+
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
+const scrollHeader = () =>{
+  const header = document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50 ? header.classList.add('bg-header') 
+                     : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+
+
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+origin: 'top',
+distance: '60px',
+duration: 2500,
+delay: 400,
+//reset:true 
+})
 
+sr.reveal('.home__data, .projects__container, .testimonial__container, .footer__container')
+sr.reveal('.home__info div', {delay: 600, origin: 'bottom', interval: 100})
+sr.reveal('.skills__content:nth-child(1), .contact__content:nth-child(1)', {origin: 'left'})
+sr.reveal('.skills__content:nth-child(2), .contact__content:nth-child(2)', {origin: 'right'})
+sr.reveal('.qualification__content, .services__card', {interval:100})
